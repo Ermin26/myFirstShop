37d20e2ac6beea0d0a79f98ebfa9cb5cae8daa82
Ermin26 <jolda.ermin@gmail.com>
2023-09-05 23:34:53 +0200

    Changed port number

--- app.js
+++ app.js
@@ -1129,6 +1129,6 @@ app.get('/other', async (req, res) => {
 
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 app.listen(port,
     console.log(`listening on ${port}`))
