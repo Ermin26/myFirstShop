f1fd79f4a20b5a249f89e0d5129cd21886249dcf
Ermin <jolda.ermin@gmail.com>
2023-12-18 22:44:02 +0100

    Added new files for legacy pages

--- utils/isLoged.js
+++ utils/isLoged.js
@@ -12,7 +12,6 @@ module.exports.isLoged = (req, res, next) => {
 
 module.exports.checkCart = (req, res, next) => {
     if (!req.session.cart) {
-
         req.session.returnTo = req.originalUrl;
         //req.flash('error', "You must add products to cart and choose payment method.");
         res.redirect('/');
