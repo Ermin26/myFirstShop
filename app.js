8f0f67f36773fd30b11c9c8444b68c8644217656
Ermin26 <68069403+Ermin26@users.noreply.github.com>
2024-03-06 15:58:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -225,7 +225,7 @@ app.post('/search', async (req, res) => {
     }
 
 })
-/*
+
 app.get('/product/:category/:subcategory/:name/:id', async (req, res) => {
     let {id } = req.params;
     console.log("This is id: ", id);
@@ -274,7 +274,7 @@ app.get('/product/:category/:subcategory/:name/:id', async (req, res) => {
         res.redirect('/');
     }
 })
-*/
+
 app.post('/add-to-cart', async (req, res) => {
     let { product_id, product_name, product_color, product_size, product_price, product_sku, invt_sku } = req.body;
     let user_id = randomUUID();
